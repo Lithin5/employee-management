8d27641bcc3dc7ac9e4a8adef88761a6bb62e7a1
Lithin5 <lithinonline@gmail.com>
2019-09-20 22:31:34 +0530

    New User Option With Profile Picture upload

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,19 @@ var router = express.Router();
 const User = require('../models/User');
 const { ensureAuthenticated, forwardAuthenticated } = require('../config/auth');
 const bcrypt = require('bcryptjs');
+var fs = require("fs");
+var multer = require('multer');
+var cpath = require('path')
+var storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, 'public/profilepictures')
+  },
+  filename: function (req, file, cb) {    
+    cb(null, file.fieldname + '-' + Date.now() + cpath.extname(file.originalname))
+  }
+})
 
+var upload = multer({ storage: storage })
 // Welcome Page
 router.get('/', forwardAuthenticated, (req, res) => res.render('index', { message: req.flash('error') }));
 
@@ -14,7 +26,7 @@ router.get('/dashboard', ensureAuthenticated, (req, res) =>
   })
 );
 router.get('/users', ensureAuthenticated, (req, res) => {
-  User.find({ "email": { $ne: req.user.name } }, function (err, users) {
+  User.find({ "email": { $ne: req.user.email } }, function (err, users) {
     res.render('access/users', {
       user: req.user,
       user_list: users
@@ -28,11 +40,14 @@ router.get('/users/addusers', ensureAuthenticated, (req, res) => {
     user: req.user
   });
 });
-router.post('/users/saveuser', ensureAuthenticated, (req, res) => {
+router.post('/users/saveuser', upload.single('file'), ensureAuthenticated, (req, res) => {
   var newuser = new User(req.body);
   newuser.password = "Password@1";
   newuser.power = "2";
   newuser.status = "active";
+  if(req.file.filename){
+    newuser.profilepicture = req.file.filename;
+  }  
   bcrypt.genSalt(10, (err, salt) => {
     bcrypt.hash(newuser.password, salt, (err, hash) => {
       if (err) throw err;
@@ -42,7 +57,7 @@ router.post('/users/saveuser', ensureAuthenticated, (req, res) => {
         .then(user => {
           req.flash(
             'success_msg',
-            'You are now registered and can log in'
+            'New User has been created, Default password is Password@1'
           );
           res.redirect('/users');
         })
