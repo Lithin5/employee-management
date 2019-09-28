50d917649a26fdef47b93ed4ae2c0f01583fb309
Lithin5 <lithinonline@gmail.com>
2019-09-28 16:40:05 +0530

    Password

--- config/keys.js
+++ config/keys.js
@@ -1,4 +1,5 @@
-dbPassword = 'mongodb://localhost:27017/employeemanager';
+//dbPassword = 'mongodb://localhost:27017/employeemanager';
+dbPassword = 'mongodb+srv://lithin:Lithin5@cluster0-istre.gcp.mongodb.net/test?retryWrites=true&w=majority'
 
 module.exports = {
     mongoURI: dbPassword
